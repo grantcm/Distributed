f6dbd7f94d912c6541580dfd9bbbc814bf8ff5a1
Grant <grantcomiller@gmail.com>
2018-04-12 14:18:57 -0400

    Added Registry Tag

--- src/server/RMIRegistry.java
+++ src/server/RMIRegistry.java
@@ -3,6 +3,10 @@ package server;
 import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
 
+import util.annotations.Tags;
+import util.tags.DistributedTags;
+
+@Tags({DistributedTags.REGISTRY, DistributedTags.RMI})
 public class RMIRegistry implements RMIValues{
 	public static void main (String[] args) {
 		try {
